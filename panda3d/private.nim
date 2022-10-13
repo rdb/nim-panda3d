6ddfe4195058905fbfb1f3171a9847fd8b0dcbbd
rdb <git@rdb.name>
2022-10-13 14:26:53 +0200

    Register assert handler in debug mode

--- panda3d/private.nim
+++ panda3d/private.nim
@@ -12,6 +12,12 @@ when defined(vcc):
   when defined(pandaDir):
     {.passC: "/I\"" & pandaDir & "/include\"".}
 
+# For memcpy, Notify
+{.emit: """
+#include <cstring>
+#include "pnotify.h"
+""".}
+
 const deconstifyCode* = """
 #include "pointerTo.h"
 
@@ -34,17 +40,13 @@ N_LIB_PRIVATE N_NIMCALL(std::string, nimStringToStdString)(struct NimStringDesc 
 N_LIB_PRIVATE N_NIMCALL(struct NimStringDesc*, nimStringFromStdString)(const std::string &s);
 """;
 
-# For memcpy
-{.emit: """
-#include <cstring>
-""".}
-
 type
   std_string* {.importcpp: "std::string", header: "string".} = object
 
 type
   std_string_const_ref* {.importcpp: "std::string const&", header: "string".} = object
 
+func size(this: std_string): int {.importcpp: "size".}
 func size(this: std_string_const_ref): int {.importcpp: "size".}
 
 func nimStringFromStdString(s: std_string_const_ref): string {.noinit, exportcpp: "nimStringFromStdString".} =
@@ -53,3 +55,19 @@ func nimStringFromStdString(s: std_string_const_ref): string {.noinit, exportcpp
 
 func nimStringToStdString(desc: string): std_string {.noinit, exportcpp: "nimStringToStdString".} =
   {.emit: "if (desc != nullptr) result.assign(desc->data, desc->len);"}
+
+when not defined(release):
+  type
+    const_char {.importcpp: "const char", nodecl.} = object
+
+  func assertHandler(expression: ptr[const_char], line: cint, sourceFile: ptr[const_char]): bool {.exportcpp: "nimAssertHandler".} =
+    var cmsg : std_string
+    {.emit: [cmsg, " = Notify::ptr()->get_assert_error_message();"].}
+    var msg = newString(cmsg.size())
+    {.emit: ["memcpy(", msg, "->data, ", cmsg, ".data(), ", cmsg, ".size());"].}
+    raiseAssert(msg)
+
+  proc installAssertHandler() =
+    {.emit: "Notify::ptr()->set_assert_handler(&nimAssertHandler);".}
+
+  installAssertHandler()

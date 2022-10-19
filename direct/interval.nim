d3b6b2e0d09d91daee82222582710f8591f9e4c6
rdb <git@rdb.name>
2022-10-19 21:05:47 +0200

    Working Func intervals

--- direct/interval.nim
+++ direct/interval.nim
@@ -6,6 +6,7 @@ export direct
 var lerpNodePathNum = 1
 var sequenceNum = 1
 var parallelNum = 1
+var funcNum = 1
 
 proc posInterval*(nodePath: NodePath, duration: float, pos: VBase3, startPos: VBase3): CLerpNodePathInterval =
   let num = lerpNodePathNum
@@ -41,3 +42,70 @@ proc Parallel*(ivals: varargs[CInterval], name: string = ""): CMetaInterval =
 
 proc Wait*(duration: float): WaitInterval =
   return newWaitInterval(duration)
+
+{.emit: """/*TYPESECTION*/
+#include "cInterval.h"
+
+class NimFunctionInterval final : public CInterval {
+public:
+  typedef void Function(void *env);
+
+  NimFunctionInterval(const std::string &name, Function *proc, void *env) :
+    CInterval(name, 0.0, true),
+    _proc(proc),
+    _env(env) {}
+  virtual ~NimFunctionInterval();
+
+  virtual void priv_instant() override {
+    _proc(_env);
+  }
+
+private:
+  Function *_proc;
+  void *_env;
+};
+""".}
+
+proc unrefIvalEnv(envp: pointer) {.noinit, exportcpp: "unrefIvalEnv".} =
+  GC_unref(cast[RootRef](envp))
+
+{.emit: """
+NimFunctionInterval::~NimFunctionInterval() {
+  if (_env != nullptr) {
+    unrefIvalEnv(_env);
+  }
+}
+""".}
+
+proc Func*(function: proc()): CInterval =
+  var procp = rawProc(function);
+  var envp = rawEnv(function);
+  if envp != nil:
+    GC_ref(cast[RootRef](envp))
+
+  let num = funcNum
+  funcNum += 1
+  let name: string = "Func-" & $num
+
+  {.emit: """
+  std::string cname;
+  if (name != nullptr) {
+    cname.assign(name->data, name->len);
+  }
+  `result` = new NimFunctionInterval(cname, (NimFunctionInterval::Function *)`procp`, `envp`);
+  """.}
+
+template Func*[T1](function: proc(arg1: T1), arg1: T1): CInterval =
+  Func(proc() = function(arg1))
+
+template Func*[T1, T2](function: proc(arg1: T1, arg2: T2), arg1: T1, arg2: T2): CInterval =
+  Func(proc() = function(arg1, arg2))
+
+template Func*[T1, T2, T3](function: proc(arg1: T1, arg2: T2, arg3: T3), arg1: T1, arg2: T2, arg3: T3): CInterval =
+  Func(proc() = function(arg1, arg2, arg3))
+
+template Func*[T1, T2, T3, T4](function: proc(arg1: T1, arg2: T2, arg3: T3, arg4: T4), arg1: T1, arg2: T2, arg3: T3, arg4: T4): CInterval =
+  Func(proc() = function(arg1, arg2, arg3, arg4))
+
+template Func*[T1, T2, T3, T4, T5](function: proc(arg1: T1, arg2: T2, arg3: T3, arg4: T4), arg1: T1, arg2: T2, arg3: T3, arg4: T4, arg5: T5): CInterval =
+  Func(proc() = function(arg1, arg2, arg3, arg4, arg5))
